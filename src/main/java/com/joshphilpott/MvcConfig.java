1f946c8f0e213ac4eff08f8e1a897b21f0f1abe0
Josh Philpott <jphil529@gmail.com>
2017-02-18 00:46:00 -0700

    Added MvcConfig and basic html templates

---
+++ src/main/java/com/joshphilpott/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.joshphilpott;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by pterodactyl on 2/18/17.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/signup").setViewName("signup");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
